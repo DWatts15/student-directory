afb2a55e66b584c40ed5a61131d93a7ec273c836
Daniel Watts <d.watts15@outlook.com>
2022-01-14 15:50:39 +0000

    Created method to allow user to input the names of the students.

--- directory.rb
+++ directory.rb
@@ -1,34 +1,43 @@
-# let's put all students into an array
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november}
-  ]
+# create a method to allow user input to fill the names in the student array
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # create an empty array
+  students = []
+  # get the first name
+  name = gets.chomp
+  # while the name is not empty, repeat this code
+  while !name.empty? do
+    # add the student hash to the array
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    # get another name from the user
+    name = gets.chomp
+  end
+  # return the array of students
+  students
+end
 
+# print the header
 def print_header
   puts "The students of Villains Academy"
   puts "-------------"
 end
 
+# print the list of students, iterating over the array to print name and cohort
 def print(students)
   students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
+#print footer
 def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
 
 #nothing happens until we call the methods
+students = input_students
 print_header
 print(students)
 print_footer(students)
